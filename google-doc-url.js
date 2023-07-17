8a87f5f7b796df58d6362c820bb77ae4ddd25378
fortehealth <139818538+fortehealth@users.noreply.github.com>
2023-07-17 14:45:37 -0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1M1fII_Nspg7frow8lpN_2OMJ60sgf4NMtRVCrRPxB-U/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyD9EyGsk_lahxv7dEQfT_lO9IwGNyWXHhg';
+var googleApiKey = 'AIzaSyAoUrLvulxpy1eVSDSmFU4IyM0H3DAeexM';
