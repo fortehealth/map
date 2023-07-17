f6db7c1e83fe183896e151fc9665f1823a723275
fortehealth <139818538+fortehealth@users.noreply.github.com>
2023-07-17 14:40:03 -0700

    Add files via upload

---
+++ google-doc-url.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1SuJHMD4eqx8FxsycdxWLiLXsel9aHdnGfaoRO3J9OG8/edit#gid=0';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyD9EyGsk_lahxv7dEQfT_lO9IwGNyWXHhg';
